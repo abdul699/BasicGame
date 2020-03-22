e8e3585484aaedf7db02efde8af0b2880c2b939b
Abdul Salam Khan <kabdulsalam6@gmail.com>
2020-03-23 00:15:48 +0530

    Added ball, bricks
    Added ball, bricks, and collision of ball with paddle

--- src/paddle.js
+++ src/paddle.js
@@ -1,14 +1,14 @@
 export default class Paddle {
-  constructor(gameWidth, gameHeight) {
-    this.gameWidth = gameWidth;
+  constructor(game) {
+    this.gameWidth = game.gameWidth;
     this.width = 150;
     this.height = 30;
     this.maxSpeed = 5;
     this.speed = 0;
 
     this.position = {
-      x: gameWidth / 2 - this.width / 2,
-      y: gameHeight - this.height - 10
+      x: game.gameWidth / 2 - this.width / 2,
+      y: game.gameHeight - this.height - 10
     };
   }
 
@@ -30,7 +30,6 @@ export default class Paddle {
   }
 
   update(deltatime) {
-    if (!deltatime) return;
     this.position.x += this.speed;
     if (this.position.x < 0) this.position.x = 0;
     if (this.position.x + this.width > this.gameWidth)
