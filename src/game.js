d20e67c36381ef2dd72f143845d775ece82f55d8
Abdul Salam Khan <kabdulsalam6@gmail.com>
2020-04-01 00:46:56 +0530

    Increased level speed
    Increased level speed from level 1 and changed brick image.

--- src/game.js
+++ src/game.js
@@ -42,6 +42,10 @@ export default class Game {
       this.gameState !== GAMESTATE.NEWLEVEL
     )
       return;
+
+    if (this.currentLevel >= 1) {
+      this.ball.speed = { x: 4, y: -4 };
+    }
     // build the level
     this.bricks = buildLevel(this, this.levels[this.currentLevel]);
     this.ball.reset();
